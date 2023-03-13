1aa5c19131ba99d0188f22fd8e2c995bffa682f6
OlgaPitkevica <krentale@gmail.com>
2023-03-13 16:36:52 +0200

    Updated tasks week2

--- Tasks 020323/Task 3.py
+++
@@ -1,9 +0,0 @@
-int1 = input("Please write one number:")
-int2 = input("Please write second number:")
-a = int(int1)
-b = int(int2)
-result = a%2==0 and b%2==0
-print("Both numbers are even:" + str(result))
-
-result = a%2==0 or b%2==0
-print("At least one number is even:" + str(result))
--- Tasks 020323/Task 5.py
+++
@@ -1,2 +0,0 @@
-date = int(input ("Please enter date (dd,mm,yyyy):"))
-print("Date valid:"+ str())
